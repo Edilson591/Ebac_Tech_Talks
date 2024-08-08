96f0ea43ab75ae437b61bb80ebd0a183941f3690
Edilson Luciano <edison591@gmail.com>
2024-08-08 10:53:07 -0300

    cirando a contagem regressiva

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -584,7 +584,28 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"3cYfC":[function(require,module,exports) {
-console.log("ola");
+AOS.init();
+const dataDoEvento = new Date("Sep 08, 2024 09:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(()=>{
+    const agora = new Date();
+    const timeStampDoAtual = agora.getTime();
+    const diasEmMs = 86400000;
+    const horasEmMs = 3600000;
+    const minutosEmMs = 60000;
+    const segundoEmMs = 1000;
+    const distanciaEvento = timeStampDoEvento - timeStampDoAtual;
+    const diasAteEvento = Math.floor(distanciaEvento / diasEmMs);
+    const horasEvent = Math.floor(distanciaEvento % diasEmMs / horasEmMs);
+    const minutosEvent = Math.floor(distanciaEvento % diasEmMs / minutosEmMs % 60);
+    const segundosEvent = Math.floor(distanciaEvento % minutosEmMs / segundoEmMs);
+    const results = document.getElementById("results");
+    if (segundosEvent > 0) results.innerHTML = `${diasAteEvento}d ${horasEvent}h ${minutosEvent}m ${segundosEvent}s`;
+    else {
+        clearInterval(contaAsHoras);
+        results.innerHTML = "0d 0h 0m 0s";
+    }
+}, 1000);
 
 },{}]},["8bDoD","3cYfC"], "3cYfC", "parcelRequireec61")
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
-console.log("ola")
+AOS.init();
+
+
+const dataDoEvento = new Date("Sep 08, 2024 09:00:00")
+const timeStampDoEvento = dataDoEvento.getTime()
+
+
+
+const contaAsHoras = setInterval( () => {
+    const agora = new Date();
+    const timeStampDoAtual = agora.getTime();
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+    const segundoEmMs = 1000;
+
+    const distanciaEvento = timeStampDoEvento - timeStampDoAtual;
+    const diasAteEvento = Math.floor(distanciaEvento / diasEmMs);
+    const horasEvent = Math.floor((distanciaEvento % diasEmMs) / horasEmMs);
+    const minutosEvent = Math.floor((distanciaEvento % diasEmMs) / minutosEmMs % 60);
+    const segundosEvent = Math.floor((distanciaEvento % minutosEmMs) / segundoEmMs);
+
+
+    const results = document.getElementById("results")
+    if(segundosEvent > 0) {
+        results.innerHTML = `${diasAteEvento}d ${horasEvent}h ${minutosEvent}m ${segundosEvent}s`;
+
+    }else {
+        clearInterval(contaAsHoras);
+        results.innerHTML = 'Evento Expirado';
+    }
+
+
+
+},1000)
+
